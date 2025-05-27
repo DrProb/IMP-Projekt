5e86fcff55917e795916a4bdf78ca83eb5b61842
DrProb <leo.scheller@student.englisches-institut.eu>
2025-05-27 20:02:56 +0200

    Fenster wird geöffnet

---
+++ Programmcode.py
@@ -0,0 +1,20 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("Fenster")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit
